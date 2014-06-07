555ac61272f095e26f2660036fdbef2b3b85dd1f
Tarcísio Nunes <tarcnux@tarcnux.com.br>
2014-06-07 12:02:19 -0300

    Primeiro commit no Netbeans

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Aprender PHP</title>
+    </head>
+    <body>
+        <?php
+        echo "<h1>Aprender PHP é legal pra valer</h1>";
+        echo "<p>bahhhhhhhhh</p>";
+        ?>
+    </body>
+</html>
